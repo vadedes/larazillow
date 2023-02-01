1f06aa76b0c0c5cae4660b08225abc85022eea50
Dave <dave@vadedesign.com>
2023-02-01 22:28:43 +0800

    Added resource controller and configured listing controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index() {
-        return inertia('Index/Index');
+        return inertia('Index/Index', [
+            'message' => 'Hello from Laravel'
+            ]
+        );
     }
 
     public function show() {
