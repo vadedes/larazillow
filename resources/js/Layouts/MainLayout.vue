fea787ea80a73b8a8bdd5acb025909777c40a03a
Dave <87390532+vadedes@users.noreply.github.com>
2023-02-03 12:21:57 +0800

    Added flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,31 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div>The page with time {{ timer }}</div>
+  <Link href="/listing">Listings</Link>
+  <Link href="/listing/create">Add Listing</Link>
+  <!-- <div>The page with time {{ timer }}</div> -->
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
   <slot>Default</slot>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/vue3'
-import {ref} from 'vue'
+import {Link, usePage} from '@inertiajs/vue3'
+import { computed } from 'vue'
 
-const timer = ref(0)
-setInterval(()=> timer.value++, 1000)
+//access flash message data
+const flashSuccess = computed(() => usePage().props.flash.success)
+
+// import {ref} from 'vue'
+// const timer = ref(0)
+// setInterval(()=> timer.value++, 1000)
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+
+</style>
